ac7ac5d40868e5903ea27bc7a27d14748087351d
ryan_on_windows10 <2922437656@qq.com>
2022-11-07 12:27:58 +0800

    增加了按键切换音乐,关闭音乐的功能,通过一个共享过来的全局变量实现

--- board/buzz_music.c
+++ board/buzz_music.c
@@ -9,16 +9,35 @@
 #define u8 uint8_t
 #define uc16 unsigned int
 #define PBeep(x) HAL_GPIO_WritePin(GPIOE,GPIO_PIN_5,(x))
-#define CPU_FREQUENCY_MHZ    8	// STM32时钟主频
-static int play_flag=0;
-void enable_play(void){
-    play_flag=1;
-}
-void disable_play(void){
-    play_flag=0;
-}
+#define CPU_FREQUENCY_MHZ    6	// STM32时钟主频
+extern int buzz_play_flag;
+//             低7  1   2   3   4   5   6   7  高1 高2  高3 高4 高5 不发音
+const uc16 tone[] = {247,262,294,330,349,392,440,494,523,587,659,698,784,850,1000};//音频数据表
 
-void set_up(void){
+//小燕子
+const u8 music[]={2,1,2,1,2,3,5,3,
+                  2,1,2,1,2,3,2,1,6,
+                  2,1,2,1,2,3,5,3,
+                  2,3,2,1,2,0,
+                  2,1,2,1,2,3,5,3,
+                  2,3,2,1,6,3,2,1,2
+};
+
+const u8 time[] ={6,2,6,2,4,4,4,4,//时间
+                  6,2,6,2,2,2,2,8,
+                  6,2,6,2,4,4,4,4,
+                  6,2,4,4,8,8,
+                  6,2,6,2,4,4,4,4,
+                  6,2,4,4,8,2,2,2,2
+};
+//void enable_play(void){
+//    buzz_play_flag=1;
+//}
+//void disable_play(void){
+//    buzz_play_flag=0;
+//}
+
+void buzz_init(void){
     GPIO_InitTypeDef GPIO_InitStruct = {0};
     GPIO_InitStruct.Pin = GPIO_PIN_5;
     GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
@@ -76,35 +95,28 @@ void Sound(u16 frq)
 
 void play(void)
 {
-    //             低7  1   2   3   4   5   6   7  高1 高2  高3 高4 高5 不发音
-    uc16 tone[] = {247,262,294,330,349,392,440,494,523,587,659,698,784,850,1000};//音频数据表
-
-    //小燕子
-    u8 music[]={2,1,2,1,2,3,5,3,
-                2,1,2,1,2,3,2,1,6,
-                2,1,2,1,2,3,5,3,
-                2,3,2,1,2,0,
-                2,1,2,1,2,3,5,3,
-                2,3,2,1,6,3,2,1,2
-    };
-
-    u8 time[] ={6,2,6,2,4,4,4,4,//时间
-                6,2,6,2,2,2,2,8,
-                6,2,6,2,4,4,4,4,
-                6,2,4,4,8,8,
-                6,2,6,2,4,4,4,4,
-                6,2,4,4,8,2,2,2,2
-    };
-
-
     u32 yanshi;
     u16 i,e;
     yanshi = 6;//10;  4;  2
-    set_up();
-    for(i=0;i<46;i++){
-        for(e=0;e<((u16)time[i])*tone[music[i]]/yanshi;e++){
-            Sound((u32)tone[music[i]]);
+
+    if(buzz_play_flag == 0){
+        for(i=0;i<46;i++){
+            for(e=0;e<((u16)time[i])*tone[music[i]]/yanshi;e++){
+                if(buzz_play_flag != 0)
+                    return;
+                Sound((u32)tone[music[i]]);
+            }
         }
     }
+    else if(buzz_play_flag == 1){
+        for(i=45;i>0;i--){
+            for(e=0;e<((u16)time[i])*tone[music[i]]/yanshi;e++){
+                if(buzz_play_flag != 1)
+                    return;
+                Sound((u32)tone[music[i]]);
+            }
+        }
+    }
+
 }
 
