7f65344b23515a18dee483351b9b45a60b43977a
ryan_on_windows10 <2922437656@qq.com>
2022-11-08 12:25:42 +0800

    移除了重复定义的宏

--- board/smg.c
+++ board/smg.c
@@ -6,21 +6,6 @@
 #include "stdio.h"
 
 #define TUBE_OFF 37 //数码段表熄灭的下标
-#define SEL0 GPIO_PIN_0 //译码器A0
-#define SEL1 GPIO_PIN_1 //译码器A1
-#define SEL2 GPIO_PIN_2 //译码器A2
-#define LED_SEL GPIO_PIN_3 //LED灯开关
-#define L0 GPIO_PIN_8  //LED0 (数码段的A)
-#define L1 GPIO_PIN_9
-#define L2 GPIO_PIN_10
-#define L3 GPIO_PIN_11
-#define L4 GPIO_PIN_12
-#define L5 GPIO_PIN_13
-#define L6 GPIO_PIN_14
-#define L7 GPIO_PIN_15
-#define LOCK_GROUP GPIOB //LED灯开关的引脚的组
-#define LIGHT_GROUP GPIOE //LED灯的引脚的组
-#define ALL_LED L0|L1|L2|L3|L4|L5|L6|L7
 
 const short int code_table[]={
         0x3F,  //"0"
