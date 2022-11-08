51b9c7becffca44a61d9585cb2831964448d0127
ryan_on_windows10 <2922437656@qq.com>
2022-11-08 13:18:11 +0800

    把温度误差和阻值换成了宏,方便修改

--- board/my_adc.c
+++ board/my_adc.c
@@ -2,10 +2,12 @@
 #include "math.h"
 extern ADC_HandleTypeDef hadc1;
 
+#define R_rt 10000.0
+#define TemperatureOffset -3.4
 float calculate_to_temperature(int ad){
     float v=((float)ad)/4095*3.3;
-    float r=(v*100000.0)/(3.3-v);//10kΩ
-    float t=(1/(log(r/100000.0)/3950.0+(1/(273.15+25.0))))-273.15-3.4;
+    float r=(v*R_rt)/(3.3-v);//10kΩ
+    float t=(1/(log(r/R_rt)/3950.0+(1/(273.15+23.0))))-273.15+TemperatureOffset;
     return  t;
 }
 
